18cdde3f1af4fec68e58bad8d31f80c4c6c853f2
v-k-1-8 <lakhiyanivarun14@gmail.com>
2023-04-15 20:03:05 +0530

    FOS

--- lib/order_details.dart
+++ lib/order_details.dart
@@ -11,6 +11,7 @@ class odetalis extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           leading: IconButton(
